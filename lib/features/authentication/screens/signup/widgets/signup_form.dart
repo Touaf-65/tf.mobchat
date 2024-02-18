8592bd1b2358846aecf220fb2d715f4e245bdac3
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-17 19:28:02 -0500

    Implementation of Sign up form

---
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../../utils/constants/text_strings.dart';
+
+class SignUpForm extends StatelessWidget {
+  SignUpForm({super.key});
+
+  @override
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Form(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 32,
+              horizontal: 25
+            ),
+          child: Column(
+            children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.tertiary
+                  )
+                ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.primary
+                    ),
+                  ),
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                  filled: true,
+                  prefixIcon: const Icon(Iconsax.direct_right),
+                  labelText: AppTexts.email,
+                  hintText: AppTexts.emailHint,
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            /// Password
+              TextFormField(
+                controller: _pwController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                          color: Theme.of(context).colorScheme.tertiary
+                      )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary
+                    ),
+                  ),
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                  filled: true,
+                  prefixIcon: const Icon(Iconsax.password_check),
+                  labelText: AppTexts.password,
+                  hintText: AppTexts.passwordHint,
+                  suffixIcon: const Icon(Iconsax.eye_slash),
+                ),
+              ),
+
+            const SizedBox(
+              height: 16,
+            ),
+
+            /// Confirm Password
+              TextFormField(
+                controller: _pwController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                          color: Theme.of(context).colorScheme.tertiary
+                      )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary
+                    ),
+                  ),
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                  filled: true,
+                  prefixIcon: const Icon(Iconsax.password_check),
+                  labelText: AppTexts.cfpassword,
+                  hintText: AppTexts.cfpasswordHint,
+                  suffixIcon: const Icon(Iconsax.eye_slash),
+                ),
+              ),
+
+            const SizedBox(
+              height: 32,
+            ),
+
+            GestureDetector(
+              onTap: () => Get.to(() => Container()),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                margin: EdgeInsets.symmetric(horizontal: 80),
+                child: Center(
+                  child: Text(AppTexts.register),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 32,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Already have an account? ',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary
+                ),),
+                Text('Login',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary
+                ),
+                ),
+              ],
+            )
+            ],
+          ),
+        ),
+    );
+  }
+}
