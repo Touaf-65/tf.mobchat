d33dc88b60cc685c03c3f6004ee9acfd3d8b23ef
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-19 15:35:29 -0500

    Adding and configuring firebase in the project

---
+++ lib/utils/constants/api.dart
@@ -0,0 +1,37 @@
+class Apikeys {
+  Apikeys._();
+
+  // Web
+  static const String webApiKey = 'AIzaSyA_YZ4SUcoGhQAO4nnuEoe7HpEtj17NM70';
+  static const String webAppId = '1:623574827883:web:db1cab6e2f74973ee2c22e';
+  static const String webMessagingSenderId = '623574827883';
+  static const String webProjectId = 'mobchat-8b836';
+  static const String webAuthDomain = 'mobchat-8b836.firebaseapp.com';
+  static const String webStorageBucket = 'mobchat-8b836.appspot.com';
+
+  // android
+  static const String androidApiKey = 'AIzaSyBZknZA8KXAqjjJXj54jrF177KkHCTwwiI';
+  static const String androidAppId = '1:623574827883:android:83afa3d46599baf6e2c22e';
+  static const String androidMessagingSenderId = '623574827883';
+  static const String androidProjectId = 'mobchat-8b836';
+  static const String androidStorageBucket = 'mobchat-8b836.appspot.com';
+
+
+  // ios
+  static const String iosApiKey = 'AIzaSyDlWwgXXppCvc7Q8dX8DSrWm-3HxkYqZ5A';
+  static const String iosAppId = '1:623574827883:ios:bec024ce78b1a8f4e2c22e';
+  static const String iosMessagingSenderId = '623574827883';
+  static const String iosProjectId = 'mobchat-8b836';
+  static const String iosStorageBucket = 'mobchat-8b836.appspot.com';
+  static const String iosBundleId = 'com.example.mobilechat';
+
+
+  // ios
+  static const String macApiKey = 'AIzaSyDlWwgXXppCvc7Q8dX8DSrWm-3HxkYqZ5A';
+  static const String macAppId = '1:623574827883:ios:24c0cc756121229be2c22e';
+  static const String macMessagingSenderId = '623574827883';
+  static const String macProjectId = 'mobchat-8b836';
+  static const String macStorageBucket = 'mobchat-8b836.appspot.com';
+  static const String macBundleId = 'com.example.mobilechat.RunnerTests';
+
+}
